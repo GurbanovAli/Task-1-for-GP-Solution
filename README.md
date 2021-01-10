d9ae1dd02e81af9adf2616c3d4ffd80e286024fa
GurbanovAli <aligurbanov95@mail.ru>
2021-01-10 18:20:25 +0300

    add files for gh-page

--- README.md
+++
@@ -1,2 +0,0 @@
-# Task-1-for-GP-Solution
-Create news feed with add, edit, delete buttons and search engine news.
--- src/news-page/interfaces/Data.ts
+++
@@ -1,7 +0,0 @@
-export interface Data {
-  id: number;
-  title: string;
-  theme: string;
-  date: string;
-  description: string;
-};
